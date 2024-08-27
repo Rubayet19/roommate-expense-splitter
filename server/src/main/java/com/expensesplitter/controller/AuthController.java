20464bb6a1410b493ecaf10105a3270b3ecebaf1
Rubayet <skrbm361@gmail.com>
2024-08-27 14:37:22 -0700

    Update AuthController.java

--- server/src/main/java/com/expensesplitter/controller/AuthController.java
+++ server/src/main/java/com/expensesplitter/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*; 
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
