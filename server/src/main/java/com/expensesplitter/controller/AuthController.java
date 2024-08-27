1374da94c27f9225ae96a55c69812edb1fa975b4
Rubayet <skrbm361@gmail.com>
2024-08-27 14:01:15 -0700

    Update AuthController.java

--- server/src/main/java/com/expensesplitter/controller/AuthController.java
+++ server/src/main/java/com/expensesplitter/controller/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000","https://roommate-expense-splitter.netlify.app")
+@CrossOrigin(origins = {"http://localhost:3000", "https://roommate-expense-splitter.netlify.app"})
 
 public class AuthController {
     private final AuthenticationManager authenticationManager;
