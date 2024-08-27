63587a7d16c22295191ef57ab871fde7dba39ac7
Rubayet <skrbm361@gmail.com>
2024-08-27 11:50:20 -0700

    Update authService.ts

--- client/app/services/authService.ts
+++ client/app/services/authService.ts
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_URL = '${process.env.NEXT_PUBLIC_API_URL}/auth/';
+const API_URL = process.env.NEXT_PUBLIC_API_URL + '/auth/';
 
 export const login = async (username: string, password: string) => {
   try {
