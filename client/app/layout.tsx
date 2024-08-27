44d7a22cb65deab2dab1690fc3cc0489f7e1a4ff
Rubayet <skrbm361@gmail.com>
2024-08-26 22:02:28 -0700

    Update layout.tsx

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,25 +1,19 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { GoogleOAuthProvider } from './GoogleOAuthProvider';
-
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css'
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "Roommate Expense Splitter",
-  description: "A simple and easy-to-use application that helps you manage your shared living expenses.",
-};
+  title: 'Roommate Expense Splitter',
+  description: 'Manage your shared living expenses easily',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <GoogleOAuthProvider>
-        <body className={`${inter.className} p-4`}>{children}</body>
-      </GoogleOAuthProvider>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
